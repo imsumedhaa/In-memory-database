802b0d531df3af4f3671f254c5a69d1204041678
Sumedha <codewithsumedha@gmail.com>
2025-06-30 18:23:21 +0530

    Write all test for http

--- api/http_test.go
+++ api/http_test.go
@@ -51,7 +51,7 @@ func TestHttp_Create_Failure(t *testing.T) {
 	mockClient := mocks.NewClient(t) //Creates a mock version of your postgres.Client interface.
 
 	// Simulate DB error
-	mockClient.On("CreatePostgresRow", "Hello", "World").Return(errors.New("server error")).Once()
+	mockClient.On("CreatePostgresRow", "Hello", "World").Return(errors.New("db error")).Once()
 
 	handler := &Http{client: mockClient}
 
@@ -69,7 +69,7 @@ func TestHttp_Create_Failure(t *testing.T) {
 	handler.create(rec, req)
 
 	assert.Equal(t, http.StatusInternalServerError, rec.Code, "Expected status 500")
-	assert.Contains(t, rec.Body.String(), "Failed to create row: server error")
+	assert.Contains(t, rec.Body.String(), "Failed to create row: db error")
 
 	mockClient.AssertExpectations(t)
 }
@@ -100,7 +100,183 @@ func TestHttp_Update_Success(t *testing.T) {
 	err := json.NewDecoder(rec.Body).Decode(&actualResponse)
 
 	assert.NoError(t, err)
-	assert.Equal(t,expectedResponse,actualResponse)
+	assert.Equal(t, expectedResponse, actualResponse)
 
 	mockClient.AssertExpectations(t)
+}
+
+func TestHttp_Update_Failure(t *testing.T) {
+
+	mockClient := mocks.NewClient(t)
+	mockClient.On("UpdatePostgresRow", "Hello", "World").Return(errors.New("db error")).Times(1)
+
+	handler := &Http{client: mockClient}
+
+	reqBody := Request{
+		Key:   "Hello",
+		Value: "World",
+	}
+	byteReqBody, _ := json.Marshal(reqBody)
+
+	req := httptest.NewRequest(http.MethodPut, "/update", bytes.NewBuffer(byteReqBody))
+	req.Header.Set("Content-Type", "application/json")
+
+	rec := httptest.NewRecorder()
+	handler.update(rec, req)
+
+	assert.Equal(t, http.StatusInternalServerError, rec.Code, "Expected status code 500")
+	assert.Contains(t, rec.Body.String(), "Failed to update row: db error")
+
+	mockClient.AssertExpectations(t)
+
+}
+
+func TestHttp_Delete_Success(t *testing.T) {
+
+	mockClient := mocks.NewClient(t) //NewClient creates a new instance of Client. It also registers a testing interface on the mock
+	mockClient.On("DeletePostgresRow", "Hello").Return(nil).Times(1)
+
+	handler := &Http{client: mockClient}
+
+	reqBody := Request{
+		Key: "Hello",
+	}
+
+	byteReqBody, _ := json.Marshal(reqBody)
+	req := httptest.NewRequest(http.MethodDelete, "/delete", bytes.NewBuffer(byteReqBody))
+	req.Header.Set("Content-Type", "application/json")
+
+	rec := httptest.NewRecorder()
+	handler.delete(rec, req)
+
+	assert.Equal(t, http.StatusOK, rec.Code, "Expected status code 200")
+
+	expectedResponse := Response{Message: "Row deleted succesfully"}
+
+	var actualResponse Response
+	err := json.NewDecoder(rec.Body).Decode(&actualResponse)
+
+	assert.NoError(t, err)
+	assert.Equal(t, expectedResponse, actualResponse)
+	mockClient.AssertExpectations(t)
+}
+
+func TestHttp_Delete_Failure(t *testing.T) {
+
+	mockClient := mocks.NewClient(t)
+	mockClient.On("DeletePostgresRow", "Hello").Return(errors.New("db error")).Times(1)
+
+	handler := &Http{client: mockClient}
+
+	reqBody := Request{
+		Key: "Hello",
+	}
+	byteReqBody, _ := json.Marshal(reqBody)
+	req := httptest.NewRequest(http.MethodDelete, "/delete", bytes.NewBuffer(byteReqBody))
+	req.Header.Set("Content-Type", "application/json")
+
+	rec := httptest.NewRecorder()
+	handler.delete(rec, req)
+
+	assert.Equal(t, http.StatusInternalServerError, rec.Code, "expected status code 500")
+
+	assert.Contains(t, rec.Body.String(), "Failed to delete row: db error")
+	mockClient.AssertExpectations(t)
+}
+
+func TestHttp_Get_Success(t *testing.T) {
+
+	mockClient := mocks.NewClient(t)
+	mockClient.On("GetPostgresRow", "Hello").Return("World", nil).Times(1)
+
+	handler := &Http{client: mockClient}
+
+	reqBody := Request{
+		Key: "Hello",
+	}
+	byteReqBody, _ := json.Marshal(reqBody)
+	req := httptest.NewRequest(http.MethodGet, "/get", bytes.NewBuffer(byteReqBody))
+	req.Header.Set("Content-Type", "application/json")
+
+	rec := httptest.NewRecorder()
+
+	handler.get(rec, req)
+	assert.Equal(t, http.StatusOK, rec.Code, "Expected status code 200")
+
+	expectedResponse := map[string]string{
+		"key":   "Hello",
+		"value": "World",
+	}
+	var actualResponse map[string]string
+	err := json.NewDecoder(rec.Body).Decode(&actualResponse)
+
+	assert.NoError(t, err)
+	assert.Equal(t, expectedResponse, actualResponse)
+	mockClient.AssertExpectations(t)
+}
+
+func TestHttp_Get_Failure(t *testing.T) {
+
+	mockClient := mocks.NewClient(t)
+	mockClient.On("GetPostgresRow", "Hello").Return("", errors.New("db error")).Times(1)
+
+	handler := &Http{client: mockClient}
+
+	reqBody := Request{
+		Key: "Hello",
+	}
+	byteReqBody, _ := json.Marshal(reqBody)
+	req := httptest.NewRequest(http.MethodGet, "/get", bytes.NewBuffer(byteReqBody))
+	req.Header.Set("Content-Type", "application/json")
+
+	rec := httptest.NewRecorder()
+
+	handler.get(rec, req)
+	assert.Equal(t, http.StatusInternalServerError, rec.Code, "Expected 500")
+
+	assert.Contains(t, rec.Body.String(), "Failed to get the row: db error")
+	mockClient.AssertExpectations(t)
+}
+
+func TestHttp_Show_Success(t *testing.T) {
+
+	mockClient := mocks.NewClient(t)
+	expectedResponse := map[string]string{
+
+		"Key":   "Hello",
+		"Value": "World",
+	}
+	mockClient.On("ShowPostgresRow").Return(expectedResponse, nil).Times(1)
+
+	handler := &Http{client: mockClient}
+
+	req := httptest.NewRequest(http.MethodGet, "/show", nil)
+	rec := httptest.NewRecorder()
+	handler.show(rec, req)
+	assert.Equal(t, http.StatusOK, rec.Code)
+
+	var actualResponse map[string]string
+	err := json.NewDecoder(rec.Body).Decode(&actualResponse)
+
+	assert.NoError(t, err)
+	assert.Equal(t, expectedResponse, actualResponse)
+	mockClient.AssertExpectations(t)
+}
+
+func TestHttp_Show_Failure(t *testing.T) {
+
+	mockClient := mocks.NewClient(t)
+	mockClient.On("ShowPostgresRow").Return(nil, errors.New("db error")).Times(1)
+
+	handler := &Http{client: mockClient}
+
+	req := httptest.NewRequest(http.MethodGet, "/show", nil)
+	rec := httptest.NewRecorder()
+
+	handler.show(rec, req)
+	assert.Equal(t,http.StatusInternalServerError,rec.Code,"Expected status code 500")
+
+	assert.Contains(t, rec.Body.String(),"Failed to show row: db error")
+	mockClient.AssertExpectations(t)
+
 }
